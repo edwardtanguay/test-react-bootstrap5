ffe258676dee45931ceb346c11f4be1df5dce0e6
Edward Tanguay (hm.win) <edward@tanguay.info>
2022-03-25 08:41:24 +0100

    with form

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
-import { Button } from 'react-bootstrap';
+import { Container, Button } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.scss'
+import { Login } from './components/Login';
 
 function App() {
 	return (
 		<div className="App">
-			<h1>Info Site</h1>
-			<p>Welcome to this site.</p>
-			<Button variant="success">Get Info</Button>
-			<Button className="ms-2" variant="danger">Stop Process</Button>
+			<Container>
+				<h1>Info Site</h1>
+				<p>Welcome to this site.</p>
+				<Button variant="success">Get Info</Button>
+				<Button className="ms-2" variant="danger">Stop Process</Button>
+				<Login />
+			</Container>
 		</div>
 	)
 }
